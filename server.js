30392124685ab203b11de3e340a9d8ab0e80710e
Kristin Pflug <pflugkf@gmail.com>
2024-05-06 20:57:53 -0400

    troubleshooting issues on frontend from hosting backend

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const path = require("path");
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-type,Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   next();
 });
 
